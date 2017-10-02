4af4c8770d57d4420d2cc718bf619e82ab27a848
Ezequiel Barrios <ezequiel@barrios.com.ar>
2017-10-02 00:08:10 -0300

    Conjunto con mostrar

--- labo6/src/Conjunto.h
+++ labo6/src/Conjunto.h
@@ -77,6 +77,11 @@ public:
      */
     size_t cardinal() const;
 
+    friend std::ostream &operator<<(std::ostream &os, Conjunto<T> &c) {
+        return c.mostrar(os);
+    }
+
+    std::ostream &mostrar(std::ostream &) const;
 
 private:
     ABB<T> _abb;
@@ -84,90 +89,10 @@ private:
     const T &_siguienteEn(const T &, ABB<T> &);
 
     bool _todosExistenEn(const ABB<T> &, const ABB<T> &) const;
+
+    std::ostream &mostrar(std::ostream &, const ABB<T> &) const;
 };
 
-template<class T>
-bool Conjunto<T>::operator==(const Conjunto &otro) const {
-    const ABB<T> &abbOtro = otro._abb;
-    return _todosExistenEn(_abb, abbOtro) and _todosExistenEn(abbOtro, _abb);
-}
-
-template<class T>
-bool Conjunto<T>::_todosExistenEn(const ABB<T> &abb, const ABB<T> &abbOtro) const {
-    if(not abbOtro.existe(abb.raiz())){
-        return false;
-    }
-    if(abb.tieneSubarbolIzquierdo() and not _todosExistenEn(abb.subarbolIzquierdo(), abbOtro)){
-        return false;
-    }
-    if(abb.tieneSubarbolDerecho() and not _todosExistenEn(abb.subarbolDerecho(), abbOtro)){
-        return false;
-    }
-    return true;
-}
-
-template<class T>
-void Conjunto<T>::insertar(const T &elem) {
-    if (not _abb.existe(elem)) {
-        _abb.insertar(elem);
-    }
-}
-
-template<class T>
-bool Conjunto<T>::pertenece(const T &elem) const {
-    return _abb.existe(elem);
-}
-
-template<class T>
-const T &Conjunto<T>::siguiente(const T &elem) {
-    return _siguienteEn(elem, _abb);
-}
-
-template<class T>
-const T &Conjunto<T>::_siguienteEn(const T &elem, ABB<T> &abb) {
-    if (elem == abb.raiz()) {
-        if (abb.tieneSubarbolDerecho()) {
-            return abb.subarbolDerecho().minimo();
-        }
-    }
-    if (elem > abb.raiz()) {
-        if (abb.tieneSubarbolDerecho()) {
-            return _siguienteEn(elem, abb.subarbolDerecho());
-        } else {
-            return elem;
-        }
-    } else {
-        if (abb.tieneSubarbolIzquierdo()) {
-            const T &siguienteEnIzquierdo = _siguienteEn(elem, abb.subarbolIzquierdo());
-            if (siguienteEnIzquierdo == elem) {
-                return abb.raiz();
-            } else {
-                return siguienteEnIzquierdo;
-            }
-        } else {
-            return abb.raiz();
-        }
-    }
-}
-
-template<class T>
-void Conjunto<T>::remover(const T &elem) {
-    _abb.remover(elem);
-}
-
-template<class T>
-const T &Conjunto<T>::minimo() const {
-    return _abb.minimo();
-}
-
-template<class T>
-const T &Conjunto<T>::maximo() const {
-    return _abb.maximo();
-}
-
-template<class T>
-std::size_t Conjunto<T>::cardinal() const {
-    return _abb.cantidad();
-}
+#include "Conjunto.inl"
 
 #endif //ALGO2_LABO_CLASE3_CONJUNTO_H
